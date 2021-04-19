4875e6324c51df60cdba79a69646ab4b6bb8d279
bogdan <bodyaua1984@gmail.com>
2021-04-19 13:40:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.arow-right',
+      prevEl: '.arow-left',
+    },
+  });
